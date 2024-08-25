08c2e066b4b45e4eee5eac774c9d206841ea003b
Michael Johnson <kojo.johnson44@gmail.com>
2024-08-25 21:54:28 +0000

    design polish

--- app/components/Receipt.js
+++ app/components/Receipt.js
@@ -1,10 +1,10 @@
 import Image from "next/image";
 import Logo from "../../public/emergent_logo.png";
 import Name from "../../public/emergent_name.png";
-import giftBox from "../../public/gift-box.png";
-import { MdOutlineLocationSearching } from "react-icons/md";
-import { TfiFaceSmile } from "react-icons/tfi";
-import { VscReport } from "react-icons/vsc";
+// import giftBox from "../../public/gift-box.png";
+// import { MdOutlineLocationSearching } from "react-icons/md";
+// import { TfiFaceSmile } from "react-icons/tfi";
+// import { VscReport } from "react-icons/vsc";
 import { FaCircleCheck } from "react-icons/fa6";
 
 export default function Receipt() {
@@ -20,7 +20,7 @@ export default function Receipt() {
           <div className="flex p-2">
             <Image
               src={Logo}
-              className="h-[2rem] w-[2.5rem]"
+              className="h-[2.2rem] w-[2.5rem]"
               alt="Emergent Payment logo"
               priority
             />
@@ -48,7 +48,7 @@ export default function Receipt() {
         </div>
         <div className="flex justify-between items-center">
           <h3>Status</h3>
-          <div className="flex justify-center items-center py-1 rounded-md gap-1 w-[6rem] bg-[#00cc8e] text-white">
+          <div className="flex justify-center items-center py-1 rounded-md gap-1 w-[6rem] bg-[#FF9A00] text-white">
             <FaCircleCheck />
             <h3 className="text-[14px]">Success</h3>
           </div>
@@ -57,14 +57,14 @@ export default function Receipt() {
           <table className="w-full">
             <thead className="border-y">
               <tr>
-                <th className="text-left">Qty</th>
+                <th className="text-left"></th>
                 <th>Description</th>
                 <th className="text-right">Amount</th>
               </tr>
             </thead>
             <tbody className="text-center">
               <tr>
-                <td className="text-left">x1</td>
+                <td className="text-left"></td>
                 <td>Paid via *789*5*0#</td>
                 <td className="text-right">GHS 400.00</td>
               </tr>
@@ -81,7 +81,7 @@ export default function Receipt() {
                 <td className="text-right">GHS 404.00</td>
               </tr>
               <tr className="border-y">
-                <td className="flex flex-row items-center gap-1">
+                {/* <td className="flex flex-row items-center gap-1">
                   <Image
                     src={giftBox}
                     alt="gift-box"
@@ -90,30 +90,13 @@ export default function Receipt() {
                   <h2>Reward Points</h2>
                 </td>
                 <td></td>
-                <td className="text-right">0 gpts</td>
+                <td className="text-right">0 gpts</td> */}
               </tr>
             </tfoot>
           </table>
         </div>
-        <div className="flex flex-row justify-around my-5">
-          <div className="flex flex-col justify-center items-center gap-1">
-            <MdOutlineLocationSearching size={20} />
-            <p>Track</p>
-          </div>
-          <div className="flex flex-col justify-center items-center gap-1">
-            <TfiFaceSmile size={20} />
-            <p>Rate</p>
-          </div>
-          <div className="flex flex-col justify-center items-center gap-1">
-            <VscReport size={20} />
-            <p>Report</p>
-          </div>
-        </div>
       </main>
-      <p className="text-center my-9">
-        Do more from your phone, shopping, payment orders, receipts and rewards.
-        All in one app
-      </p>
+      <p className="text-center my-9">Powered by Emergent Payments Africa</p>
     </section>
   );
 }
