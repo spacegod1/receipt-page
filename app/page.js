ea2fb5ef2b893442945b1cd510578ecf84b10c0c
Michael Johnson <kojo.johnson44@gmail.com>
2026-02-23 15:07:04 +0000

    other changes

--- app/page.js
+++ app/page.js
@@ -46,9 +46,9 @@ export default function Home() {
               <Image
                 src="/full emergent logo.png"
                 alt="Emergent Payments"
-                width={120}
-                height={40}
-                className="h-9 w-auto object-contain"
+                width={160}
+                height={54}
+                className="h-12 w-auto object-contain"
                 priority
               />
             </div>
